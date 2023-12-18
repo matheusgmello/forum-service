2c3c90c394e8a00eb261e5b3bef66205def243d7
Matheus Gabriel <matheusgmello1@gmail.com>
2023-12-18 20:50:50 -0300

    Refactor:  Comunicação entre camadas

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -5,6 +5,7 @@ import { CreateAccountController } from './controllers/create-account.controller
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
 import { DatabaseModule } from '../database/database.module'
+import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
 
 @Module({
   imports: [DatabaseModule],
@@ -14,6 +15,6 @@ import { DatabaseModule } from '../database/database.module'
     CreateQuestionController,
     FetchRecentQuestionsController,
   ],
-  providers: [],
+  providers: [CreateQuestionUseCase],
 })
 export class HttpModule {}
