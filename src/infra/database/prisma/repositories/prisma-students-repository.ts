dd414b09c0c57c050a2fa8bf4063c69acb64fa9a
Matheus Gabriel <matheusgmello1@gmail.com>
2023-12-23 11:48:47 -0300

    Feat:  Implementando repositórios

--- src/infra/database/prisma/repositories/prisma-students-repository.ts
+++ src/infra/database/prisma/repositories/prisma-students-repository.ts
@@ -1,7 +1,7 @@
-import { StudentsRepository } from '@/domain/forum/application/repositories/students-repository'
-import { Student } from '@/domain/forum/enterprise/entities/student'
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from '../prisma.service'
+import { StudentsRepository } from '@/domain/forum/application/repositories/students-repository'
+import { Student } from '@/domain/forum/enterprise/entities/student'
 import { PrismaStudentMapper } from '../mappers/prisma-student-mapper'
 
 @Injectable()
