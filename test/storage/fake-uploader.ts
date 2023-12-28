15ea83e7bc502fb3c5d526f85c8a6fad2f755303
Matheus Gabriel <matheusgmello1@gmail.com>
2023-12-28 16:10:30 -0300

    Feat: Integração com Cloudflare R2

--- test/storage/fake-uploader.ts
+++ test/storage/fake-uploader.ts
@@ -1,7 +1,7 @@
 import {
   UploadParams,
   Uploader,
-} from '@/domain/forum/application/use-cases/storage/uploader'
+} from '@/domain/forum/application/storage/uploader'
 import { randomUUID } from 'crypto'
 
 interface Upload {
